19f529709fa4dee440fbadbe82fa8126182276f9
riansins <syarulote@gmail.com>
2015-04-02 21:52:23 +0900

    update ca2c file

---
+++ DY/NET/LSIS/Glopa.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DY.NET.LSIS
+{
+    public class Glopa
+    {
+        public static bool IsGlopaVar(string glopa_var_name)
+        {
+            return glopa_var_name[0] == '%';
+        }
+
+        public static DataType GetDataType(string glopa_var_name)
+        {
+            if (!IsGlopaVar(glopa_var_name))
+                throw new ArgumentException("it's not glopa variable name");
+
+            char device = glopa_var_name[1];
+            char type = glopa_var_name[2];
+
+            DataType ret = DataType.WORD;
+
+            switch(type)
+            {
+                case 'X':
+                    ret = DataType.BIT;
+                    break;
+                case 'B':
+                    ret = DataType.BYTE;
+                    break;
+                case 'W':
+                    ret = DataType.WORD;
+                    break;
+                case 'D':
+                    ret = DataType.DWORD;
+                    break;
+                case 'L':
+                    ret = DataType.LWORD;
+                    break;
+                default:
+                    throw new Exception("glopa variable name wrong");
+                    break;
+            }
+            return ret;
+        }
+    }
+}
---
+++ DY/NET/LSIS/LD2Glopa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DY.NET.LSIS
+{
+    public class LD2Glopa
+    {
+        public static string VarConvert(string ld, DataType type)
+        {
+            string ret = ld;
+            string type_s = null ;
+            switch (type)
+            {
+                case DataType.BIT:
+                    type_s = "X";
+                    break;
+                case DataType.BYTE:
+                    type_s = "B";
+                    break;
+                case DataType.WORD:
+                    type_s = "W";
+                    break;
+                case DataType.DWORD:
+                    type_s = "D";
+                    break;
+                case DataType.LWORD:
+                    type_s = "L";
+                    break;
+            }
+            ret.Insert(0, "%");
+            ret.Insert(2, type_s);
+            return ret;
+        }
+    }
+}
