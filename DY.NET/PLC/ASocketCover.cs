5c7029b5ba1968f8312837f265be086f94806495
CHILD_KIM <riansins@icloud.com>
2015-08-22 09:53:44 +0900

    lsis xgt itimeout

--- DY.NET/PLC/ASocketCover.cs
+++ DY.NET/PLC/ASocketCover.cs
@@ -3,6 +3,7 @@
  * 기능: 통신에 필요한 최소한의 기능들을 인터페이스로 구현
  * 날짜: 2015-03-25
  */
+
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -20,23 +21,22 @@ namespace DY.NET
         private static Logger LOG = LogManager.GetCurrentClassLogger();
         private readonly AsyncLock m_AsyncLock = new AsyncLock();
 
+        private int m_ReadTimeout;
+        private int m_ReadTimeoutMaximum;
+        private int m_ReadTimeoutMinimum;
+
+        private int m_WriteTimeout;
+        private int m_WriteTimeoutMaximum;
+        private int m_WriteTimeoutMinimum;
+
         protected readonly byte[] EMPTY_BYTE = new byte[1];
         //BUFFER
         public const int BUFFER_SIZE = 4096;
         protected byte[] BaseBuffer = new byte[BUFFER_SIZE];
-        protected int BufferIndex;
+        //protected int BufferIndex;
         //소켓 스트림
         protected Stream BaseStream { get; set; }
-        //타임아웃
-        public int WriteTimeout { get; set; }
-        public int ReadTimeout { get; set; }
-
-        public int WriteTimeoutMaximum { get; set; }
-        public int WriteTimeoutMinimum { get; set; }
-
-        public int ReadTimeoutMaximum { get; set; }
-        public int ReadTimeoutMinimum { get; set; }
-
+        
         //ITAG
         public int Tag { get; set; }
         public string Description { get; set; }
@@ -45,6 +45,84 @@ namespace DY.NET
         /// Connect, Close 이벤트 발생 시 호출
         /// </summary>
         public EventHandler<ConnectionStatusChangedEventArgs> ConnectionStatusChanged { get; set; }
+
+        public int ReadTimeoutMaximum
+        {
+            get
+            {
+                return m_ReadTimeoutMaximum;
+            }
+            set
+            {
+                if (0 <= value && value >= ReadTimeoutMinimum)
+                    m_ReadTimeoutMaximum = value;
+            }
+        }
+
+        public int ReadTimeoutMinimum
+        {
+            get
+            {
+                return m_ReadTimeoutMinimum;
+            }
+            set
+            {
+                if (0 <= value && value <= ReadTimeoutMaximum)
+                    m_ReadTimeoutMinimum = value;
+            }
+        }
+
+        public int WriteTimeoutMaximum
+        {
+            get
+            {
+                return m_WriteTimeoutMaximum;
+            }
+            set
+            {
+                if (0 <= value && ReadTimeoutMinimum <= value)
+                    m_WriteTimeoutMaximum = value;
+            }
+        }
+
+        public int WriteTimeoutMinimum
+        {
+            get
+            {
+                return m_WriteTimeoutMinimum;
+            }
+            set
+            {
+                if (0 <= value && value <= WriteTimeoutMaximum)
+                    m_WriteTimeoutMinimum = value;
+            }
+        }
+
+        public int WriteTimeout
+        {
+            get
+            {
+                return m_WriteTimeout;
+            }
+            set
+            {
+                if (WriteTimeoutMinimum <= value && value <= WriteTimeoutMaximum)
+                    m_WriteTimeout = value;
+            }
+        }
+
+        public int ReadTimeout
+        {
+            get
+            {
+                return m_ReadTimeout;
+            }
+            set
+            {
+                if (ReadTimeoutMinimum <= value && value <= ReadTimeoutMaximum)
+                    m_ReadTimeout = value;
+            }
+        }
 
         public abstract bool Connect();
         public abstract void Close();
@@ -57,14 +135,14 @@ namespace DY.NET
         /// </summary>
         protected ASocketCover()
         {
-            WriteTimeout = 250;
-            ReadTimeout = 250;
-
             WriteTimeoutMaximum = 500;
             ReadTimeoutMaximum = 500;
 
             WriteTimeoutMinimum = 0;
             ReadTimeoutMinimum = 0;
+
+            WriteTimeout = 250;
+            ReadTimeout = 250;
         }
 
         /// <summary>
@@ -77,8 +155,6 @@ namespace DY.NET
             Delivery delivery = new Delivery();
             using (await m_AsyncLock.LockAsync())
             {
-                await BaseStream.FlushAsync();
-                BufferIndex = 0;
                 AProtocol p = request as AProtocol;
                 if (!IsConnected()) //CONNECT
                 {
@@ -86,25 +162,23 @@ namespace DY.NET
                     if (!Connect())
                         return delivery.Packing(DeliveryError.DISCONNECT);
                 }
-                CancellationTokenSource write_cts = new CancellationTokenSource(WriteTimeout);
-                CancellationTokenSource read_cts = new CancellationTokenSource(ReadTimeout);
-                //WRITE
-                CancellationToken write_ct = write_cts.Token;
-                await BaseStream.WriteAsync(p.ASCIIProtocol, 0, p.ASCIIProtocol.Length, write_ct);
-                if (write_cts.IsCancellationRequested)
+                CancellationTokenSource wcts = new CancellationTokenSource(WriteTimeout);
+                await BaseStream.WriteAsync(p.ASCIIProtocol, 0, p.ASCIIProtocol.Length, wcts.Token);
+                if (wcts.IsCancellationRequested)
                     return delivery.Packing(DeliveryError.WRITE_TIMEOUT);
-                //READ
+
+                int idx = 0;
+                CancellationTokenSource rcts = new CancellationTokenSource(ReadTimeout);
                 do
                 {
-                    CancellationToken read_ct = read_cts.Token;
-                    int read_size = await BaseStream.ReadAsync(BaseBuffer, BufferIndex, BUFFER_SIZE - BufferIndex, read_ct);
-                    if (read_cts.IsCancellationRequested)
+                    int read_size = await BaseStream.ReadAsync(BaseBuffer, idx, BUFFER_SIZE - idx, rcts.Token);
+                    if (rcts.IsCancellationRequested)
                         return delivery.Packing(DeliveryError.READ_TIMEOUT);
                     if (read_size <= 0)
                         System.Diagnostics.Debug.Assert(false);
-                    BufferIndex += read_size;
+                    idx += read_size;
                 } while (DoReadAgain(p));
-                byte[] recv_data = new byte[BufferIndex];
+                byte[] recv_data = new byte[idx];
                 Buffer.BlockCopy(BaseBuffer, 0, recv_data, 0, recv_data.Length);
                 delivery.Package = CreateResponseProtocol(p, recv_data);
             }
